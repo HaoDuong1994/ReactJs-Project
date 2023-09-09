570dc55e304b853bce7cb75dfae1564c790d1323
HaoDuong1994 <hao.dv5370@gmail.com>
2023-09-10 05:34:07 +0700

    update

--- MindX-project/src/Component/LoginGoogle/GoogleLogin.jsx
+++ MindX-project/src/Component/LoginGoogle/GoogleLogin.jsx
@@ -1,15 +1,47 @@
+import "./Login.css";
 function GoogleLogin(props) {
   const { logInWithGoogle } = props;
   console.log(logInWithGoogle);
   return (
-    <div>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          logInWithGoogle();
-        }}>
-        Mời bạn đăng nhập google trước
-      </button>
+    <div className="authen-container">
+      <div className="sign-up-wrapper">
+        <form>
+          <h4>Đăng ký</h4>
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            placeholder="Nhập ID"
+          />
+          <br />
+          <input
+            placeholder="Nhập password"
+            type="password"
+            id="password"
+            name="password"
+          />{" "}
+          <br />
+        </form>
+      </div>
+      <div className="sign-in-wrapper">
+        <form>
+          <h4>Đăng nhập</h4>
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            placeholder="Nhập ID"
+          />
+          <br />
+          <input
+            placeholder="Nhập password"
+            type="password"
+            id="password"
+            name="password"
+          />{" "}
+          <br />
+        </form>
+      </div>
     </div>
   );
 }
