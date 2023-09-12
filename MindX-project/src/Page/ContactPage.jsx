07adad1f182e34d86d46cedc050f6cca87b090d7
HaoDuong1994 <hao.dv5370@gmail.com>
2023-09-12 16:48:48 +0700

    update

--- MindX-project/src/Page/ContactPage.jsx
+++ MindX-project/src/Page/ContactPage.jsx
@@ -1,5 +1,28 @@
 function ContactPage() {
-  return <div>Hello from Contact page</div>;
+  return (
+    <div style={{ padding: "10px" }}>
+      <h2>Liên hệ</h2>
+      <div>
+        <p>Store 1: Số 243 Nguyễn Thị Thập, Tân Phú, Q7 </p>
+        <p>ĐT: 02866602868</p>
+      </div>
+      <br></br>
+      <div>
+        <p>Store 2: Số 334 Quang Trung, P.10, Q. Gò Vấp </p>
+        <p>ĐT: 02822618333 </p>
+      </div>
+      <br></br>
+      <div>
+        <p>Store 3: Số 71 Nguyễn Trãi, P.2, Q.5 </p>
+        <p>ĐT: 02822604333 </p>
+      </div>
+      <br></br>
+      <div>
+        <p>Store 4: Số 8 Hồ Văn Huê, P.9, Q.Phú Nhuận </p>
+        <p>ĐT: 02822618333 </p>
+      </div>
+    </div>
+  );
 }
 
 export default ContactPage;
